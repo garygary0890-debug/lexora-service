aef9dd1e13c82301802aaf23ce857386f242f0a7
garygary0890-debug <garygary0890@gmail.com>
2026-09-15 20:00:44 +0700

    SRV-000018: add reports route

--- core/navigation/src/main/java/com/lexora/service/core/navigation/Routes.kt
+++ core/navigation/src/main/java/com/lexora/service/core/navigation/Routes.kt
@@ -1,6 +1,7 @@
 package com.lexora.service.core.navigation
 
 object Routes {
+    const val Reports = "reports"
     const val Documents = "documents"
     const val FieldWork = "fieldwork"
     const val Requests = "requests"

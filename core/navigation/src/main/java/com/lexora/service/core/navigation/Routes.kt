86b45735455d9256be3daccf746cfccb53498830
garygary0890-debug <garygary0890@gmail.com>
2026-09-15 14:08:47 +0700

    SRV-000005 Import navigation routes

---
+++ core/navigation/src/main/java/com/lexora/service/core/navigation/Routes.kt
@@ -0,0 +1,10 @@
+package com.lexora.service.core.navigation
+
+object Routes {
+    const val Vehicles = "vehicles"
+    const val Clients = "clients"
+    const val Home = "home"
+    const val Settings = "settings"
+    const val Wash = "wash"
+    const val Tires = "tires"
+}

aa7ef97e6dc6d9b6d92fa432529fc6e32b7b7bcd
garygary0890-debug <garygary0890@gmail.com>
2026-09-15 14:06:07 +0700

    SRV-000005 Import project structure

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LexoraService"
+include(":app")
+include(":core:designsystem")
+include(":core:model")
+include(":core:navigation")
+include(":core:data")
+include(":core:domain")
+include(":feature:home")
+include(":feature:settings")
+include(":feature:wash")
+include(":feature:tires")
+
+include(":core:database")
+include(":feature:clients")
+include(":feature:vehicles")

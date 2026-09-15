cd9122467155cafa6dbc882377aaf365be4dcb98
garygary0890-debug <garygary0890@gmail.com>
2026-09-15 14:19:25 +0700

    SRV-000006 Add assets navigation route

--- core/navigation/src/main/java/com/lexora/service/core/navigation/Routes.kt
+++ core/navigation/src/main/java/com/lexora/service/core/navigation/Routes.kt
@@ -1,6 +1,7 @@
 package com.lexora.service.core.navigation
 
 object Routes {
+    const val Assets = "assets"
     const val Vehicles = "vehicles"
     const val Clients = "clients"
     const val Home = "home"

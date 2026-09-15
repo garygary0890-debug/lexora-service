73fa0b32f5f849301b8d5d75dfff869fae41a6b1
garygary0890-debug <garygary0890@gmail.com>
2026-09-15 14:08:16 +0700

    SRV-000005 Import access models

---
+++ core/model/src/main/java/com/lexora/service/core/model/AccessModels.kt
@@ -0,0 +1,29 @@
+package com.lexora.service.core.model
+
+enum class UserRole {
+    ADMIN,
+    MANAGER,
+    DISPATCHER,
+    TECHNICIAN,
+    ACCOUNTANT,
+    AUDITOR,
+}
+
+enum class Permission {
+    VIEW_HOME,
+    MANAGE_ORGANIZATION,
+    MANAGE_USERS,
+    MANAGE_MODULES,
+    VIEW_WASH,
+    VIEW_TIRES,
+    MANAGE_WASH,
+    MANAGE_TIRES,
+    VIEW_AUDIT,
+}
+
+data class ServiceUser(
+    val id: String,
+    val displayName: String,
+    val roles: Set<UserRole>,
+    val organizationIds: Set<String>,
+)

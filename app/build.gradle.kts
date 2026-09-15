cbf45ab0d9d729a30373ad2108ee0723e2be9e43
garygary0890-debug <garygary0890@gmail.com>
2026-09-15 18:47:37 +0700

    SRV-000014 bump app to 0.8.0 and add requests module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,14 +11,15 @@ android {
         applicationId = "com.lexora.service"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.7.0"
+        versionCode = 8
+        versionName = "0.8.0"
     }
 
     buildFeatures { compose = true }
 }
 
 dependencies {
+    implementation(project(":feature:requests"))
     implementation(project(":feature:organization"))
     implementation(project(":feature:assets"))
     implementation(project(":feature:vehicles"))

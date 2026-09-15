f1094d56101b05efb0efd2a2b3d88086f8da0f40
garygary0890-debug <garygary0890@gmail.com>
2026-09-15 14:10:58 +0700

    SRV-000005 Import feature:wash

---
+++ feature/wash/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+android {
+    namespace = "com.lexora.service.feature.wash"
+    compileSdk = 36
+    defaultConfig { minSdk = 26 }
+    buildFeatures { compose = true }
+}
+dependencies {
+    implementation(platform("androidx.compose:compose-bom:2026.06.00"))
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui")
+}

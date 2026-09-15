c056590427b51266072cf09d39565c0d315f6367
garygary0890-debug <garygary0890@gmail.com>
2026-09-15 14:08:06 +0700

    SRV-000005 Import module access policy

---
+++ core/domain/src/main/java/com/lexora/service/core/domain/ModuleAccessPolicy.kt
@@ -0,0 +1,20 @@
+package com.lexora.service.core.domain
+
+import com.lexora.service.core.model.LexoraModuleId
+import com.lexora.service.core.model.ModuleDescriptor
+import com.lexora.service.core.model.Permission
+import com.lexora.service.core.model.ServiceUser
+
+class ModuleAccessPolicy(
+    private val accessPolicy: AccessPolicy = AccessPolicy(),
+) {
+    fun isAvailable(module: ModuleDescriptor, user: ServiceUser): Boolean {
+        if (!module.enabled || !module.licensed) return false
+        val permission = when (module.id) {
+            LexoraModuleId.CORE -> Permission.VIEW_HOME
+            LexoraModuleId.WASH -> Permission.VIEW_WASH
+            LexoraModuleId.TIRES -> Permission.VIEW_TIRES
+        }
+        return accessPolicy.can(user, permission)
+    }
+}

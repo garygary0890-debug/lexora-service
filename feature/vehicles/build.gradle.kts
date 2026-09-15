3bfd5025e8337ed5d64862398ba0e624a86c6dae
garygary0890-debug <garygary0890@gmail.com>
2026-09-15 14:11:25 +0700

    SRV-000005 Import feature:vehicles

---
+++ feature/vehicles/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+android {
+    namespace = "com.lexora.service.feature.vehicles"
+    compileSdk = 36
+    defaultConfig { minSdk = 26 }
+    buildFeatures { compose = true }
+}
+dependencies {
+    implementation(project(":core:model"))
+    implementation("androidx.compose.material3:material3:1.4.0")
+    implementation("androidx.compose.foundation:foundation:1.12.1")
+    implementation("androidx.compose.runtime:runtime:1.12.1")
+}

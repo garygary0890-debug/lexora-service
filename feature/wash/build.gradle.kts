5470c708f3a7f5f355a02b0842b6582155ecc109
garygary0890-debug <garygary0890@gmail.com>
2026-09-15 20:13:48 +0700

    SRV-000020 add wash model dependency

--- feature/wash/build.gradle.kts
+++ feature/wash/build.gradle.kts
@@ -9,6 +9,7 @@ android {
     buildFeatures { compose = true }
 }
 dependencies {
+    implementation(project(":core:model"))
     implementation(platform("androidx.compose:compose-bom:2026.06.00"))
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")

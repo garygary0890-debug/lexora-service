43d6d8287b6840db308a46809fb964f91feee750
garygary0890-debug <garygary0890@gmail.com>
2026-09-15 14:10:14 +0700

    SRV-000005 Import feature:home

---
+++ feature/home/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+android {
+    namespace = "com.lexora.service.feature.home"
+    compileSdk = 36
+    defaultConfig { minSdk = 26 }
+    buildFeatures { compose = true }
+}
+dependencies {
+    implementation(project(":core:model"))
+    implementation(platform("androidx.compose:compose-bom:2026.06.00"))
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui")
+}

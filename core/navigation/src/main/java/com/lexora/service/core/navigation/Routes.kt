331976eb4ffec38ac9c09652239090d9ad4a824d
garygary0890-debug <garygary0890@gmail.com>
2026-09-15 14:28:41 +0700

    SRV-000007 Add organization route

--- core/navigation/src/main/java/com/lexora/service/core/navigation/Routes.kt
+++ core/navigation/src/main/java/com/lexora/service/core/navigation/Routes.kt
@@ -1,6 +1,7 @@
 package com.lexora.service.core.navigation
 
 object Routes {
+    const val Organization = "organization"
     const val Assets = "assets"
     const val Vehicles = "vehicles"
     const val Clients = "clients"

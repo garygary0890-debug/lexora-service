95d6b4175ca9640c2df386068a1952163c14adc1
garygary0890-debug <garygary0890@gmail.com>
2026-09-15 21:47:57 +0700

    SRV-000026 bump version to 0.20.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.lexora.service"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.19.0"
+        versionCode = 20
+        versionName = "0.20.0"
     }
 
     buildFeatures { compose = true }

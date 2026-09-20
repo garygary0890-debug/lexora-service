3b8a36f700782129e8b7f0a23272378572e66819
garygary0890-debug <garygary0890@gmail.com>
2026-09-20 23:20:55 +0700

    feat: address push delivery to persisted device token

--- app/src/main/java/com/lexora/service/notifications/PushProvider.kt
+++ app/src/main/java/com/lexora/service/notifications/PushProvider.kt
@@ -2,18 +2,19 @@ package com.lexora.service.notifications
 
 import com.lexora.service.core.domain.PushProviderResult
 import com.lexora.service.core.model.NotificationDelivery
+import com.lexora.service.core.model.PushDeviceToken
 
 interface PushProvider {
     val name: String
     val configured: Boolean
-    suspend fun send(delivery: NotificationDelivery): PushProviderResult
+    suspend fun send(delivery: NotificationDelivery, token: PushDeviceToken): PushProviderResult
 }
 
 object NoConfiguredPushProvider : PushProvider {
     override val name: String = "not-configured"
     override val configured: Boolean = false
 
-    override suspend fun send(delivery: NotificationDelivery): PushProviderResult =
+    override suspend fun send(delivery: NotificationDelivery, token: PushDeviceToken): PushProviderResult =
         PushProviderResult.PermanentFailure("push_provider_not_configured")
 }
 

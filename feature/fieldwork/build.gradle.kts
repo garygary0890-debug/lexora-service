6e88ddad4a5c2909afac6a6ba6b32ce6fa05805a
garygary0890-debug <garygary0890@gmail.com>
2026-09-20 14:31:34 +0700

    SRV: connect fieldwork to execution and inventory data

--- feature/fieldwork/build.gradle.kts
+++ feature/fieldwork/build.gradle.kts
@@ -11,6 +11,7 @@ android {
 dependencies {
     implementation(project(":core:presentation"))
     implementation(project(":core:domain"))
+    implementation(project(":core:data"))
     implementation(project(":core:model"))
     implementation(platform("androidx.compose:compose-bom:2026.06.00"))
     implementation("androidx.compose.material3:material3")
